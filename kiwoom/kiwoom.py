d315e1720820871af45481ac626296086bb4fcef
dustjd2000 <dustjd2000@naver.com>
2020-11-30 21:37:38 +0900

    get accout info

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -1,16 +1,22 @@
 from PyQt5.QAxContainer import *
 from PyQt5.QtCore import *
+from config.errorCode import *
 
 class Kiwoom(QAxWidget):
     def __init__(self):
         super().__init__()
         print("Kiwoom init()")
 
+        ###########변수모음
         self.login_event_loop = None
+        self.account_num = None
+        ###################
 
         self.get_ocx_instance()
         self.event_slot()
+
         self.signal_login_commConnect()
+        self.get_account_info()
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
@@ -18,12 +24,20 @@ class Kiwoom(QAxWidget):
     def event_slot(self):
         self.OnEventConnect.connect(self.login_slot)
     
-    def login_slot(self, errCode):
-        print(errCode)
-
-        self.login_event_loop.exit()
-
+    
     def signal_login_commConnect(self):
         self.dynamicCall("CommConnect()")
         self.login_event_loop = QEventLoop()
         self.login_event_loop.exec_()
+    
+    def login_slot(self, errCode):
+        print(errors(errCode))
+        
+        self.login_event_loop.exit()
+    
+    def get_account_info(self):
+        account_list = self.dynamicCall("GetLoginInfo(String)","ACCNO")
+        
+        self.account_num = account_list.split(';')[0]
+
+        print("나의 보유계좌번호: {}".format(self.account_num))

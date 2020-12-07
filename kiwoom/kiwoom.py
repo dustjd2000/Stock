6365f2436bad77e8d36bf18584f6602952e8b30b
dustjd2000 <dustjd2000@naver.com>
2020-12-07 21:14:21 +0900

    계좌평가 잔고내역 조회

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -8,8 +8,13 @@ class Kiwoom(QAxWidget):
         print("Kiwoom init()")
 
         ###########변수모음
-        self.login_event_loop = None
         self.account_num = None
+        ###################
+
+        ###이벤트 루프 모음
+        self.login_event_loop = None
+        self.detail_account_info_event_loop = None
+        self.detail_account_info_event_loop_2 = None
         ###################
 
         self.get_ocx_instance()
@@ -18,6 +23,7 @@ class Kiwoom(QAxWidget):
         self.signal_login_commConnect()
         self.get_account_info()
         self.detail_account_info() #예수금 정보 가져오기
+        self.detail_account_mystock()   #계좌평가 잔고 내역
 
 
     def get_ocx_instance(self):
@@ -52,7 +58,20 @@ class Kiwoom(QAxWidget):
         self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매체구분", "00")
         self.dynamicCall("SetInputValue(String, String)", "조회구분", "2")
         self.dynamicCall("CommRqData(String, String, int, String)","예수금상세현황요청","opw00001", "0", "2000")
+
+        self.detail_account_info_event_loop = QEventLoop()
+        self.detail_account_info_event_loop.exec_()
     
+    def detail_account_mystock(self, sPrevNext="0"):
+        self.dynamicCall("SetInputValue(String, String)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(String, String)", "조회구분", "2")
+        self.dynamicCall("CommRqData(String, String, int, String)","계좌평가잔고내역","opw00018", sPrevNext, "2000")
+
+        self.detail_account_info_event_loop_2 = QEventLoop()
+        self.detail_account_info_event_loop_2.exec_()
+
     def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext):
         
         if sRQName == "예수금상세현황요청":
@@ -61,3 +80,18 @@ class Kiwoom(QAxWidget):
 
             ok_deposit = self.dynamicCall("GetCommData(String, String, int, String)",sTrCode, sRQName, 0, "출금가능금액")
             print("출금가능금액: {}".format(int(ok_deposit)))
+
+            self.detail_account_info_event_loop.exit()
+        
+        if sRQName == "계좌평가잔고내역":
+            total_buy_money = self.dynamicCall("GetCommData(String, String, int, String)",sTrCode, sRQName, 0, "총매입금액")
+            total_buy_money = int(total_buy_money)
+
+            print("총매입금액: {}".format(total_buy_money))
+            
+            total_profit_loss_rate = self.dynamicCall("GetCommData(String, String, int, String)",sTrCode, sRQName, 0, "총수익률(%)")
+            total_profit_loss_rate_result = float(total_profit_loss_rate)
+
+            print("총수익률(%): {}".format(total_profit_loss_rate_result))
+
+            self.detail_account_info_event_loop_2.exit()
